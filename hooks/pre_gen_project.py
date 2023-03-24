258794c053bea62107ba3cc6d96cf74dbf64b899
Daniel Cabrera <danielfct06@gmail.com>
2023-03-24 01:44:51 -0500

    Initial commit

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,17 @@
+print("Hello word")
+import os
+import sys
+
+project_new = "{{ cookiecutter.project_new }}"
+
+ERROR_COLOR = "\x1b[31m"
+MESSAGE_COLOR = "\x1b[34m"
+RESET_ALL = "\x1b[0m"
+
+if project_new.startswith("x"):
+   print(f"{ERROR_COLOR}ERROR: {project_new=} is not a valid name for this template.{RESET_ALL}")
+
+   sys.exit(1)
+
+print(f"{MESSAGE_COLOR}Let's do it! You're are going to create something awesome!")
+print(f"Creating project at { os.getcwd() }{RESET_ALL}")
